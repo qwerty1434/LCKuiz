06139df25ebd349cd139201a31a5b6b3af01c611
qwerty1434 <qwerty1434@naver.com>
2023-03-07 00:40:08 +0900

    :tada: [START] 백엔드 프로젝트 생성
    기본 디펜던시 추가한 gradle 프로젝트 생성

---
+++ backend/src/test/java/com/example/LCKuiz/LcKuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LCKuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcKuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
